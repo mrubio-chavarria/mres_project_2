279c8ae95950d5960dcece707336c4298df2b2b8
mrubio-chavarria <msrch@hotmail.es>
2021-06-10 20:45:25 +0100

    Update script to be launched in the HPC cluster.

--- flappie_script.sh
+++ flappie_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+#PBS -l select=1:ncpus=12:mem=2gb
+#PBS -l walltime=03:00:00
+#PBS -J 0-13
 
-# Name Flappie
-shopt -s expand_aliases
-alias flappie='/home/mario/Projects/flappie/flappie'
 
 # Set the current working directory
 cd "$(dirname "$0")"
@@ -10,9 +10,5 @@ cd "$(dirname "$0")"
 # Run OpenBLAS in single threaded mode
 export OPENBLAS_NUM_THREADS=1
 
-flappie --model help
-
 # Launch the model
-# flappie --model r941_native --uuid acinetobacter_data/selected_reads > acinetobacter_data/basecalled_reads/basecalled_acinetobacter.fq
-# find reads -name \*.fast5 | parallel -P $(nproc) -X flappie > basecalls.fq
-find /home/mario/Projects/project_2/databases/natural_flappie_r941_native_ap/reads -name \*.fast5 | parallel -P 3 -X /home/mario/Projects/flappie/flappie > basecalls.fq
+find $HOME/project_2/databases/natural_flappie_r941_native_ap/reads/$PBS_ARRAY_INDEX -name \*.fast5 | parallel -P $(nproc) -X $HOME/project2/flappie/flappie > $HOME/project_2/databases/natural_flappie_r941_native_ap/basecalled_reads/$PBS_ARRAY_INDEX.fq
