d5c1299fee4fd95817ae666948a213481c8b16ef
mrubio-chavarria <msrch@hotmail.es>
2021-06-11 15:46:27 +0100

    Correct route to flappie executable

--- flappie_script.sh
+++ flappie_script.sh
@@ -13,4 +13,4 @@ cd "$(dirname "$0")"
 export OPENBLAS_NUM_THREADS=1
 
 # Launch the model
-find $HOME/project_2/databases/natural_flappie_r941_native_ap/reads/$PBS_ARRAY_INDEX -name \*.fast5 | parallel -P $(nproc) -X $HOME/project2/flappie/flappie > $HOME/project_2/databases/natural_flappie_r941_native_ap/basecalled_sequences/$PBS_ARRAY_INDEX.fq
+find $HOME/project_2/databases/natural_flappie_r941_native_ap/reads/$PBS_ARRAY_INDEX -name \*.fast5 | parallel -P $(nproc) -X $HOME/flappie/flappie > $HOME/project_2/databases/natural_flappie_r941_native_ap/basecalled_sequences/$PBS_ARRAY_INDEX.fq
