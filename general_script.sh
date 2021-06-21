1e78db35be63ffa4c238109150f784a2ec6eb005
mrubio-chavarria <msrch@hotmail.es>
2021-06-21 10:24:02 +0100

    Add commands to launch through jobs

---
+++ general_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=1:mem=12gb
+#PBS -l walltime=01:00:00
+
+module load anaconda3/personal
+
+conda create --name project2_venv
+
+source activate project2_venv
+
+conda config --add channels conda-forge bioconda
+
+conda install pytorch=1.8 --yes 
+
+pip3 install ont-fast5-api
