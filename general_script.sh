5f5cecf4f93d6ca3e1924b36479161da053ee034
mrubio-chavarria <msrch@hotmail.es>
2021-07-02 09:42:02 +0100

    Update execution instructions

--- general_script.sh
+++ general_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=8:mem=12gb
+#PBS -l select=1:ncpus=8:mem=16gb
 #PBS -l walltime=01:00:00
 
 module load anaconda3/personal
 
-python3 main_parallel.py
+python3 assembly_tutorial_self.py
