8dba13234d74a8bbc541850c31a6652f8dc2c8f0
mrubio-chavarria <msrch@hotmail.es>
2021-07-04 20:43:53 +0100

    Update assembly_tutorial_self_multiprocessing_cpu.py

--- assembly_tutorial_self_multiprocessing_cpu.py
+++ assembly_tutorial_self_multiprocessing_cpu.py
@@ -498,7 +498,7 @@ if __name__ == '__main__':
         "dropout": 0.1,
         "learning_rate": 5E-4,
         "batch_size": 20,
-        "n_epochs": 5,
+        "n_epochs": 4,
         "kernel_size": 3,
         "n_kernels": 32
     }
