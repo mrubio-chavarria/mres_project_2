991abc27a86511a464e9d581b23471fd55b1994f
mrubio-chavarria <msrch@hotmail.es>
2021-07-04 12:08:29 +0100

    Update general_script.sh

--- general_script.sh
+++ general_script.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=8:mem=16gb
-#PBS -l walltime=01:00:00
+#PBS -lselect=1:ncpus=16:mem=96gb:ngpus=4
+#PBS -lwalltime=01:00:00
 
+# Load dependencies
 module load anaconda3/personal
 
-python3 $HOME/project_2/assembly_tutorial_self.py nproc
+# Check the availability of CUDA
+if [ -z "$CUDA_VISIBLE_DEVICES" ]
+then
+    echo "\$CUDA_VISIBLE_DEVICES is empty"
+    cuda_available="NO"
+else
+    echo "\$CUDA_VISIBLE_DEVICES is NOT empty"
+    echo "\$CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+    cuda_available="YES"
+fi
+
+# Launch script
+python3 $HOME/project_2/assembly_tutorial_self_multiprocessing.py cuda_available $CUDA_VISIBLE_DEVICES
