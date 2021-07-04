911acee0bdd414ba913b48d8c28f02c92d7cd86b
mrubio-chavarria <msrch@hotmail.es>
2021-07-04 13:31:33 +0100

    Add multiprocessing only with cpu for the tutoral

--- general_script.sh
+++ general_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#PBS -lselect=1:ncpus=16:mem=96gb:ngpus=4
+#PBS -lselect=1:ncpus=32:mem=64gb
 #PBS -lwalltime=01:00:00
 
 # Load dependencies
 module load anaconda3/personal
+
+source activate project2_venv
 
 # Check the availability of CUDA
 if [ -z "$CUDA_VISIBLE_DEVICES" ]
@@ -17,4 +19,4 @@ else
 fi
 
 # Launch script
-python3 $HOME/project_2/assembly_tutorial_self_multiprocessing.py cuda_available $CUDA_VISIBLE_DEVICES
+python3 $HOME/project_2/assembly_tutorial_self_multiprocessing_cpu.py
