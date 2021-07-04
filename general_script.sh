c41e6b3ccc5ae139c01462293d5a19a09d540f0b
mrubio-chavarria <msrch@hotmail.es>
2021-07-04 13:32:41 +0100

    Update general_script.sh

--- general_script.sh
+++ general_script.sh
@@ -7,16 +7,5 @@ module load anaconda3/personal
 
 source activate project2_venv
 
-# Check the availability of CUDA
-if [ -z "$CUDA_VISIBLE_DEVICES" ]
-then
-    echo "\$CUDA_VISIBLE_DEVICES is empty"
-    cuda_available="NO"
-else
-    echo "\$CUDA_VISIBLE_DEVICES is NOT empty"
-    echo "\$CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
-    cuda_available="YES"
-fi
-
 # Launch script
 python3 $HOME/project_2/assembly_tutorial_self_multiprocessing_cpu.py
