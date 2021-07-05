9fdede6a8fb0252af6ba3aaf7944f09f2a6e2a2e
mrubio-chavarria <msrch@hotmail.es>
2021-07-05 07:25:03 +0100

    Update assembly_tutorial_self_multiprocessing_cpu.py

--- assembly_tutorial_self_multiprocessing_cpu.py
+++ assembly_tutorial_self_multiprocessing_cpu.py
@@ -432,6 +432,8 @@ def train(model, train_data, test_data, parameters, device, sampler=None, rank='
             spectrograms, labels, input_lengths, label_lengths = _data
             # Compute model output
             spectrograms, labels = spectrograms.to(device), labels.to(device)
+            print(epoch)
+            print(batch_idx)
             output = model(spectrograms)  # (batch, time, n_class)
             # Correct for DataParallel output
             output = F.log_softmax(output, dim=2)
@@ -476,104 +478,6 @@ def test(model, test_loader, criterion, device):
     print('Test set: Average loss: {:.4f}, Average CER: {:4f} Average WER: {:.4f}\n'.format(test_loss, avg_cer, avg_wer))
 
 
-class CNNLayerNorm(nn.Module):
-    """Layer normalization built for cnns input"""
-    def __init__(self, n_feats):
-        super(CNNLayerNorm, self).__init__()
-        self.layer_norm = nn.LayerNorm(n_feats)
-
-    def forward(self, x):
-        # x (batch, channel, feature, time)
-        x = x.transpose(2, 3).contiguous() # (batch, channel, time, feature)
-        x = self.layer_norm(x)
-        return x.transpose(2, 3).contiguous() # (batch, channel, feature, time) 
-
-
-class ResidualCNN(nn.Module):
-    """Residual CNN inspired by https://arxiv.org/pdf/1603.05027.pdf
-        except with layer norm instead of batch norm
-    """
-    def __init__(self, in_channels, out_channels, kernel, stride, dropout, n_feats):
-        super(ResidualCNN, self).__init__()
-
-        self.cnn1 = nn.Conv2d(in_channels, out_channels, kernel, stride, padding=kernel//2)
-        self.cnn2 = nn.Conv2d(out_channels, out_channels, kernel, stride, padding=kernel//2)
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
-        self.layer_norm1 = CNNLayerNorm(n_feats)
-        self.layer_norm2 = CNNLayerNorm(n_feats)
-
-    def forward(self, x):
-        residual = x  # (batch, channel, feature, time)
-        x = self.layer_norm1(x)
-        x = F.gelu(x)
-        x = self.dropout1(x)
-        x = self.cnn1(x)
-        x = self.layer_norm2(x)
-        x = F.gelu(x)
-        x = self.dropout2(x)
-        x = self.cnn2(x)
-        x += residual
-        return x # (batch, channel, feature, time)
-
-
-class BidirectionalGRU(nn.Module):
-
-    def __init__(self, rnn_dim, hidden_size, dropout, batch_first):
-        super(BidirectionalGRU, self).__init__()
-
-        self.BiGRU = nn.GRU(
-            input_size=rnn_dim, hidden_size=hidden_size,
-            num_layers=1, batch_first=batch_first, bidirectional=True)
-        self.layer_norm = nn.LayerNorm(rnn_dim)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        x = self.layer_norm(x)
-        x = F.gelu(x)
-        x, _ = self.BiGRU(x)
-        x = self.dropout(x)
-        return x
-
-
-class SpeechRecognitionModel(nn.Module):
-    
-    def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, dropout=0.1):
-        super(SpeechRecognitionModel, self).__init__()
-        kernel_size = 16
-        n_feats = n_feats//2
-        self.cnn = nn.Conv2d(1, kernel_size, 3, stride=stride, padding=3//2)  # cnn for extracting heirachal features
-
-        # n residual cnn layers with filter size of 32
-        self.rescnn_layers = nn.Sequential(*[
-            ResidualCNN(kernel_size, kernel_size, kernel=3, stride=1, dropout=dropout, n_feats=n_feats) 
-            for _ in range(n_cnn_layers)
-        ])
-        self.fully_connected = nn.Linear(n_feats*kernel_size, rnn_dim)
-        self.birnn_layers = nn.Sequential(*[
-            BidirectionalGRU(rnn_dim=rnn_dim if i==0 else rnn_dim*2,
-                             hidden_size=rnn_dim, dropout=dropout, batch_first=i==0)
-            for i in range(n_rnn_layers)
-        ])
-        self.classifier = nn.Sequential(
-            nn.Linear(rnn_dim*2, rnn_dim),  # birnn returns rnn_dim*2
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(rnn_dim, n_class)
-        )
-
-    def forward(self, x):
-        x = self.cnn(x)
-        x = self.rescnn_layers(x)
-        sizes = x.size()
-        x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # (batch, feature, time)
-        x = x.transpose(1, 2) # (batch, time, feature)
-        x = self.fully_connected(x)
-        x = self.birnn_layers(x)
-        x = self.classifier(x)
-        return x
-
-
 if __name__ == '__main__':
 
     # Transformations to use in the data
@@ -588,8 +492,8 @@ if __name__ == '__main__':
     # Parameters for training
     parameters = {
         "in_channels": 1,
-        "n_cnn_layers": 3,
-        "n_rnn_layers": 5,
+        "n_cnn_layers": 1,
+        "n_rnn_layers": 2,
         "rnn_dim": 512, # 512,
         "n_classes": 29,
         "n_features": 128,
@@ -599,7 +503,7 @@ if __name__ == '__main__':
         "batch_size": 20,
         "n_epochs": 4,
         "kernel_size": 3,
-        "n_kernels": 16
+        "n_kernels": 2
     }
 
     # Multiprocessing settings
@@ -630,14 +534,6 @@ if __name__ == '__main__':
                     parameters['n_features'],
                     parameters['n_classes'],
                     parameters['dropout'])
-
-    # model = SpeechRecognitionModel(parameters['n_cnn_layers'], # n_cnn_layers,
-    #                             parameters['n_rnn_layers'], # n_rnn_layers,
-    #                             parameters['rnn_dim'], # rnn_dim,
-    #                             parameters['n_classes'], # n_class,
-    #                             parameters['n_features'], #n_feats,
-    #                             stride=2,
-    #                             dropout=0.1)
 
     model.to(device)
     model.share_memory()    
