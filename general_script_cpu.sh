d5abb88811bc29ef9b96307917db702bfdfa8959
mrubio-chavarria <msrch@hotmail.es>
2021-07-08 07:50:37 +0100

    Update general_script_cpu.sh

--- general_script_cpu.sh
+++ general_script_cpu.sh
@@ -8,4 +8,10 @@ module load anaconda3/personal
 source activate project2_venv
 
 # Launch script
-python3 $HOME/project_2/assembly_tutorial_self_multiprocessing_cpu.py
+# python3 $HOME/project_2/assembly_tutorial_self_multiprocessing_cpu.py
+echo "AQUI CUDA DEVICES:"
+echo $CUDA_VISIBLE_DEVICES
+echo "FIN"
+echo "AQUI CUDA DEVICES OTRA VEZ:"
+echo "$CUDA_VISIBLE_DEVICES"
+echo "FIN"
