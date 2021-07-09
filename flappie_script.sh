3061783dd9973140c1d884da5988938fbac76964
mrubio-chavarria <msrch@hotmail.es>
2021-07-09 07:00:23 +0100

    Update flappie_script.sh

--- flappie_script.sh
+++ flappie_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=32:mem=12gb
+#PBS -lselect=1:ncpus=32:mem=124gb
 #PBS -l walltime=04:00:00
 #PBS -J 0-13
 
@@ -14,4 +14,4 @@ cd "$(dirname "$0")"
 export OPENBLAS_NUM_THREADS=1
 
 # Launch the model
-find $HOME/project_2/databases/natural_flappie_r941_native_ap/reads/$PBS_ARRAY_INDEX -name \*.fast5 | parallel -P $(nproc) -X $HOME/flappie/flappie > $HOME/project_2/databases/natural_flappie_r941_native_ap/basecalled_sequences/$PBS_ARRAY_INDEX.fq
+find $HOME/project_2/databases/reference_ap/reads -name \*.fast5 | parallel -P $(nproc) -X $HOME/flappie/flappie > $HOME/project_2/databases/reference_ap/basecalls/basecalls.fq
