fd252a397fc0175dddafabcba107d9beffe14a1f
mrubio-chavarria <msrch@hotmail.es>
2021-07-09 07:38:24 +0100

    Update general_script_cpu.sh

--- general_script_cpu.sh
+++ general_script_cpu.sh
@@ -9,9 +9,4 @@ source activate project2_venv
 
 # Launch script
 # python3 $HOME/project_2/assembly_tutorial_self_multiprocessing_cpu.py
-echo "AQUI CUDA DEVICES:"
-echo $CUDA_VISIBLE_DEVICES
-echo "FIN"
-echo "AQUI CUDA DEVICES OTRA VEZ:"
-echo "$CUDA_VISIBLE_DEVICES"
-echo "FIN"
+python3 $HOME/project_2/databases/working_3xr6/preprocess_reads.py
