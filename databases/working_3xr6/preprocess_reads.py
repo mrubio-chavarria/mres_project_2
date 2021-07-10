6f04435794bd17b5a70bc30902b12d533f77b543
mrubio-chavarria <msrch@hotmail.es>
2021-07-10 07:29:32 +0100

    Update preprocess_reads.py

--- databases/working_3xr6/preprocess_reads.py
+++ databases/working_3xr6/preprocess_reads.py
@@ -49,15 +49,18 @@ def filter_reads(read_folders, filtered_reads, q_score_threshold):
                 filtered_reads.append(read_file)
 
 
-def annotate_basecalls(single_read_folders, basecalls_files, workdir):
+def annotate_basecalls(pairs, workdir):
     """
     """
     #for i in range(len(basecalls_files)):
-    for i in range(len(basecalls_files)):
-        basecall_file = workdir + '/' + 'basecalls' + '/' + basecalls_files[i]
-        command = f'tombo preprocess annotate_raw_with_fastqs --fast5-basedir {single_read_folders[i]} --fastq-filenames {basecall_file} --overwrite'
+    for pair in pairs:
+        command = f'tombo preprocess annotate_raw_with_fastqs --fast5-basedir {pair[1]} --fastq-filenames {pair[0]} --overwrite'
         os.system(command)
     
+
+def read_code(filename):
+    code1, code2 = filename.split('_')[2:4]
+    return code1[0:8] + '_' + code2
 
 if __name__ == "__main__":
     
@@ -67,28 +70,49 @@ if __name__ == "__main__":
     # workdir = f'/home/mario/Projects/project_2/databases/working_3xr6'
     # n_processes = 4
 
-    # # Format to multiple to single read files
+    # Format to multiple to single read files
     print('***************************************************************************************')
     print('Format reads from multi to single files')
     print('***************************************************************************************')
     reads_folder = workdir + '/' + 'reads'
-    # command = f'multi_to_single_fast5 --input_path {reads_folder}/multi --save_path {reads_folder}/single'
+    basecalls_folder = workdir + '/' + 'basecalls'
+    command = f'multi_to_single_fast5 --input_path {reads_folder}/multi --save_path {reads_folder}/single'
     # os.system(command)
+    # Format folder name base on file codes
+    single_reads_folder = workdir + '/reads/single'
+    file = open(single_reads_folder + '/filename_mapping.txt')
+    mapping = file.readlines()
+    file.close()
+    mapping = [line.split('\t') for line in mapping]
+    mapping = [('_'.join(line[0].split('/')[9].split('_')[1::]).replace('.fast5', ''), line[1].replace('\n', '')) 
+        for line in mapping]
+    reads_mapping = {key: [] for key in set([line[0] for line in mapping])}
+    [reads_mapping[line[0]].append(line[1]) for line in mapping]
+    folders = os.listdir(single_reads_folder)
+    for folder in folders:
+        if folder.endswith('txt'): continue
+        folder = single_reads_folder + '/' + folder
+        reads_in_folder = os.listdir(folder)
+        for key in reads_mapping.keys():
+            if reads_mapping[key][0] in reads_in_folder:
+                os.rename(folder, single_reads_folder + '/' + key)
+                break
+    file_pairs = []
+    single_reads_folders = os.listdir(single_reads_folder)
+    for file in os.listdir(basecalls_folder):
+        [file_pairs.append((basecalls_folder + '/' + file,
+                            single_reads_folder + '/' + folder))
+            for folder in single_reads_folders if folder == read_code(file)]
 
     # Annotate the reads with the basecalls
     print('***************************************************************************************')
     print('Annotate the reads')
     print('***************************************************************************************')
-    single_read_folders = [reads_folder + '/' + 'single' + '/' + folder 
-        for folder in os.listdir(reads_folder + '/' + 'single') if not folder.endswith('txt') and not folder.endswith('index')]
-    single_read_folders = sorted(single_read_folders, key=lambda x: int(x.split('/')[-1]))
-    print(list(single_read_folders))
-    print(len(list(single_read_folders)))
-    basecalls_folder = workdir + '/' + 'basecalls'
-    basecalls_files = sorted(os.listdir(basecalls_folder), key=lambda x: int(x.split('_')[3]))
-    print(list(basecalls_files))
-    print(len(list(basecalls_files)))
-    file_pairs = list(zip(single_read_folders, basecalls_files))
+    # single_read_folders = [reads_folder + '/' + 'single' + '/' + folder 
+    #     for folder in os.listdir(reads_folder + '/' + 'single') if not folder.endswith('txt') and not folder.endswith('index')]
+    # single_read_folders = sorted(single_read_folders, key=lambda x: int(x.split('/')[-1]))
+    # basecalls_files = sorted(os.listdir(basecalls_folder), key=lambda x: int(x.split('_')[3]))
+    # file_pairs = list(zip(single_read_folders, basecalls_files))
     group_size = len(file_pairs) // n_processes
     group_indeces = list(range(0, len(file_pairs), group_size))
     file_groups = [file_pairs[group_size * index:group_size * (index+1)] if index != file_pairs[group_size * index::] else index 
@@ -104,7 +128,8 @@ if __name__ == "__main__":
     print('Resquiggle the reads...')
     print('***************************************************************************************')
     reference_file = workdir + '/' + 'reference.fasta'
-    for folder in single_read_folders:
+    for folder in single_reads_folders:
+        folder = single_reads_folder + '/' + folder
         command = f'tombo resquiggle {folder} {reference_file} --processes {n_processes} --num-most-common-errors 5 --overwrite'
         os.system(command)
 
@@ -114,9 +139,9 @@ if __name__ == "__main__":
     print('***************************************************************************************')
     q_score_threshold = 20.0
     filtered_reads = []
-    n_folders_per_read = len(single_read_folders) // n_processes
-    reads_folders_lists = [single_read_folders[n_folders_per_read*i:n_folders_per_read*(i+1)] 
-        if i != (n_processes - 1) else single_read_folders[n_folders_per_read*i:] 
+    n_folders_per_read = len(single_reads_folders) // n_processes
+    reads_folders_lists = [single_reads_folders[n_folders_per_read*i:n_folders_per_read*(i+1)] 
+        if i != (n_processes - 1) else single_reads_folders[n_folders_per_read*i:] 
         for i in range(n_processes)]
     processes = []
     manager = Manager()
