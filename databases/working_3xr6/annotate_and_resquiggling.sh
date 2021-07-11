bcdbd1f0e80ce2dfbf5bb8155b07772ceb3d22e5
mrubio-chavarria <msrch@hotmail.es>
2021-07-11 13:58:32 +0100

    Update annotate_and_resquiggle_reads.py and annotate_and_resquiggling.sh

---
+++ databases/working_3xr6/annotate_and_resquiggling.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -lselect=1:ncpus=8:mem=8gb
+#PBS -J 0-24
+
+module load anaconda3/personal
+
+source activate project2_venv 
+
+cd $HOME/project_2/databases/working_3xr6
+
+# Launch the resquiggling for every folder
+python3 annotate_and_resquiggle_reads.py $HOME/project_2/databases/working_3xr6 8 flowcell2 $PBS_ARRAY_INDEX
