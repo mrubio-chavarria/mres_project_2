98f153b5c2682dc0490c5b220d61482b45104fae
mrubio-chavarria <msrch@hotmail.es>
2021-07-11 20:29:54 +0100

    Update filter_reads.sh

--- databases/working_3xr6/filter_reads.sh
+++ databases/working_3xr6/filter_reads.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=16:mem=16gb
 #PBS -lwalltime=03:00:00
+#PBS -J 1-2
+
 
 # IMPORTANT
 # - You should choose less processes than folders with single reads.
@@ -13,4 +15,9 @@ source activate project2_venv
 cd $HOME/project_2/databases/working_3xr6
 
 # Launch the resquiggling for every folder
-python3 filter_reads.py $HOME/project_2/databases/working_3xr6 12 flowcell2
+if [ $PBS_ARRAY_INDEX == 2 ]
+then
+    python3 filter_reads.py $HOME/project_2/databases/working_3xr6 12 flowcell3
+else
+    python3 filter_reads.py $HOME/project_2/databases/working_3xr6 12 flowcell1
+fi
