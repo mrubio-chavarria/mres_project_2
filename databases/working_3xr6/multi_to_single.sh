69176558aaa103869bc770b7ef16ff8f545f591f
mrubio-chavarria <msrch@hotmail.es>
2021-07-11 12:26:00 +0100

    Update multi_to_single.sh

--- databases/working_3xr6/multi_to_single.sh
+++ databases/working_3xr6/multi_to_single.sh
@@ -10,7 +10,10 @@ source activate project2_venv
 # Convert from multi to single format
 flowcell=$1
 reads_folder=$PWD/reads/$flowcell
-
+echo "***********************************"
 echo "Convert from multi to single format"
+echo "***********************************"
+echo "Input folder : $reads_folder/multi"
+echo "Output folder : $reads_folder/single"
 multi_to_single_fast5 --input_path $reads_folder/multi --save_path $reads_folder/single
 echo "Conversion completed"
