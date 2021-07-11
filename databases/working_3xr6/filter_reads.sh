3bc8e19b14a26d6777e16321ececc50e8d209e19
mrubio-chavarria <msrch@hotmail.es>
2021-07-11 18:22:52 +0100

    Update filter_reads.sh

--- databases/working_3xr6/filter_reads.sh
+++ databases/working_3xr6/filter_reads.sh
@@ -13,4 +13,4 @@ source activate project2_venv
 cd $HOME/project_2/databases/working_3xr6
 
 # Launch the resquiggling for every folder
-python3 filter_reads.py $HOME/project_2/databases/working_3xr6 16 flowcell2
+python3 filter_reads.py $HOME/project_2/databases/working_3xr6 12 flowcell2
