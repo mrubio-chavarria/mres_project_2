b44a936dc286528fd9fe878b257b300181593477
mrubio-chavarria <msrch@hotmail.es>
2021-07-13 09:23:04 +0100

    Update train_script_gpu.sh

--- train_script_gpu.sh
+++ train_script_gpu.sh
@@ -9,26 +9,36 @@ source activate project2_venv
 # Compress dataset
 echo "Compressing dataset"
 cd $HOME/project2/databases
-tar -zcvf single.tar.gz working_3xr6/reads/single
+tar -zcvf flowcell1_single.tar.gz working_3xr6/reads/flowcell1/single
+tar -zcvf flowcell2_single.tar.gz working_3xr6/reads/flowcell2/single
+tar -zcvf flowcell3_single.tar.gz working_3xr6/reads/flowcell3/single
 tar -zcvf reference.fasta.tar.gz working_3xr6/reference.fasta
 echo "Compression completed"
 
 # Send dataset
 echo "Transfering dataset to node"
 mkdir $TMPDIR/project_2/databases
-mv single.tar.gz $TMPDIR/project_2/databases/single.tar.gz
+mv flowcell1_single.tar.gz $TMPDIR/project_2/databases/flowcell1_single.tar.gz
+mv flowcell2_single.tar.gz $TMPDIR/project_2/databases/flowcell2_single.tar.gz
+mv flowcell3_single.tar.gz $TMPDIR/project_2/databases/flowcell3_single.tar.gz
 mv reference.fasta.tar.gz $TMPDIR/project_2/databases/reference.fasta.tar.gz
 echo "Transference completed"
 
 # Extract dataset
 echo "Extracting dataset"
 cd $TMPDIR/project_2/databases
-tar -xzvf single.tar.gz
+tar -xzvf flowcell1_single.tar.gz
+tar -xzvf flowcell2_single.tar.gz
+tar -xzvf flowcell3_single.tar.gz
 tar -xzvf reference.fasta.tar.gz
 
 # Format the file structure
-mkdir working_3xr6/reads/single
-mv single working_3xr6/reads/single
+mkdir working_3xr6/reads/flowcell1/single
+mkdir working_3xr6/reads/flowcell2/single
+mkdir working_3xr6/reads/flowcell3/single
+mv flowcell1_single/* working_3xr6/reads/flowcell1/single
+mv flowcell2_single/* working_3xr6/reads/flowcell2/single
+mv flowcell3_single/* working_3xr6/reads/flowcell3/single
 mv reference.fasta working_3xr6/reference.fasta
 echo "Extraction completed"
 
