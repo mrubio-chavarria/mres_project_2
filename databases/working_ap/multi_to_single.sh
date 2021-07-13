8a4b0d537629d7a168a762d954340e4409ae2661
mrubio-chavarria <msrch@hotmail.es>
2021-07-13 21:08:05 +0100

    Add working_ap/multi_to_single.sh

---
+++ databases/working_ap/multi_to_single.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -lselect=1:ncpus=1:mem=4gb
+#PBS -lwalltime=03:00:00
+#PBS -J 1-3
+
+# Load dependencies
+module load anaconda3/personal
+
+source activate project2_venv
+
+# Convert from multi to single format
+flowcell="flowcell$PBS_ARRAY_INDEX"
+reads_folder=$HOME/project_2/databases/working_ap/reads/$flowcell
+echo "*****************************************"
+echo "Convert fast5 from multi to single format"
+echo "*****************************************"
+echo "Input folder : $reads_folder/multi"
+echo "Output folder : $reads_folder/single"
+multi_to_single_fast5 --input_path $reads_folder/multi --save_path $reads_folder/single
+echo "Conversion completed"
+
+echo "******************************************"
+echo "Convert fastq from single to multi format"
+echo "******************************************"
+cd $HOME/project_2/databases/working_ap/basecalls/$flowcell
+rm multi.fastq
+cat *.fastq > multi.fastq
+echo "Merged file: multi.fastq" 
+echo "Conversion completed"
