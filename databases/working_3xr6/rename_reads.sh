8e4f6d6c70399badad976ff9abdb7e7485ffc663
mrubio-chavarria <msrch@hotmail.es>
2021-07-15 13:08:03 +0100

    Update rename_reads.sh

--- databases/working_3xr6/rename_reads.sh
+++ databases/working_3xr6/rename_reads.sh
@@ -9,7 +9,7 @@ source activate project2_venv
 
 cd $HOME/project_2/databases/working_ap
 
-for flowcell in flowcell1 flowcell2 flowcell3 flowcell4
+for flowcell in flowcell1 flowcell2 flowcell3
 do
   python3 rename_reads.py $HOME/project_2/databases/working_ap $flowcell
 done
