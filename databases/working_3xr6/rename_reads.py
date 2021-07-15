ab691f1b8cde2f5b5c3772f2e870e5a34b568417
mrubio-chavarria <msrch@hotmail.es>
2021-07-15 12:45:10 +0100

    Update rename_reads.py and rename_reads.sh

---
+++ databases/working_3xr6/rename_reads.py
@@ -0,0 +1,57 @@
+#!/home/mario/anaconda3/envs/project2_venv/bin python
+
+"""
+DESCRIPTION:
+This script tries to gather all the steps needed to 
+perform once the basecalls have been obtained.
+"""
+
+# Libraries
+import os
+import sys
+from tombo import tombo_helper, tombo_stats, resquiggle
+import h5py
+import mappy
+from tqdm import tqdm
+import multiprocessing as mp
+from multiprocessing import Manager
+from shutil import move, rmtrees
+
+
+if __name__ == "__main__":
+    
+    workdir = sys.argv[1]
+    flowcell = sys.argv[2]
+    
+    # workdir = f'/home/mario/Projects/project_2/databases/working_3xr6'
+    # n_processes = 2
+    # flowcell = 'flowcell3'
+
+    # Filter files below the q score threshold
+    print('***************************************************************************************')
+    print('Rename read files')
+    print('Flowcell:', flowcell)
+    print('***************************************************************************************')
+    single_reads_folders = os.listdir(workdir + '/' + 'reads' + '/' + flowcell + '/' + 'single')
+    single_reads_folders = [
+        workdir + '/' + 'reads' + '/' + flowcell + '/' + 'single' + '/' + folder 
+        for folder in single_reads_folders if not folder.endswith('txt')
+    ]
+    # Rename files
+    for folder in single_reads_folders:
+        for idx, file in enumerate(os.listdir(folder)):
+            if not file.endswith('fast5'):
+                continue
+            old_filename = folder + '/' + file
+            new_filename = folder + '/' + f'read{str(idx)}.fast5'
+            os.rename(old_filename, new_filename)
+    print('Renaming completed')
+        
+
+            
+    
+
+    
+    
+
+
