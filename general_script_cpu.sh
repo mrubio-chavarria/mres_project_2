b5ee7a339b17722e2a910d8a70db057725b9313b
mrubio-chavarria <msrch@hotmail.es>
2021-07-15 19:32:03 +0100

    Update general_script_cpu.sh

--- general_script_cpu.sh
+++ general_script_cpu.sh
@@ -8,6 +8,6 @@ module load anaconda3/personal
 source activate project2_venv
 
 # Launch script
-# python3 $HOME/project_2/assembly_tutorial_self_multiprocessing_cpu.py
-echo $nproc
-python3 $HOME/project_2/databases/working_3xr6/preprocess_reads.py $HOME/project_2 16 flowcell3
+cd $HOME/project_2/databases
+rm -r working_3xr6
+cp -R working_3xr6_security working_3xr6
