f93e10fae6acdb6b7f80ad805be6bd905ab3e17e
mrubio-chavarria <msrch@hotmail.es>
2021-07-16 17:15:58 +0100

    Update general_script_cpu_2.sh and general_script_ont_cpu.sh

--- general_script_cpu_2.sh
+++ general_script_cpu_2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=32:mem=124gb
 #PBS -lwalltime=01:00:00
+#PBS -J 1-3
 
 # Load dependencies
 module load anaconda3/personal
@@ -9,11 +10,7 @@ source activate project2_venv
 
 cd $HOME/project_2/databases
 
-
-for flowcell in flowcell1 flowcell2 flowcell3
-do
-    rm -r working_3xr6/reads/$flowcell/single_security
-    cp -R working_3xr6/reads/$flowcell/single working_3xr6/reads/$flowcell/single_security
-done
+rm -r working_3xr6/reads/flowcell$PBS_ARRAY_INDEX/single_security
+cp -R working_3xr6/reads/flowcell$PBS_ARRAY_INDEX/single working_3xr6/reads/flowcell$PBS_ARRAY_INDEX/single_security
 
 
