3ef376d2fc62ed7fded554ddaccd8ae22315d327
mrubio-chavarria <msrch@hotmail.es>
2021-07-16 08:14:08 +0100

    Update filtering, one job per flowcell

--- databases/working_ap/filter_reads.sh
+++ databases/working_ap/filter_reads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=4:mem=4gb
 #PBS -lwalltime=12:00:00
-#PBS -J 0-44
+#PBS -J 1-4
 
 module load anaconda3/personal
 
@@ -9,9 +9,9 @@ source activate project2_venv
 
 cd $HOME/project_2/databases/working_ap
 
+flowcell=flowcell$PBS_ARRAY_INDEX
+
 # Launch the resquiggling for every folder
 echo "Job index: $PBS_ARRAY_INDEX"
-for flowcell in flowcell1 flowcell2 flowcell3 flowcell4
-do
-    python3 filter_reads.py $HOME/project_2/databases/working_ap 45 $flowcell $PBS_ARRAY_INDEX
-done
+echo "Flocell: $flowcell"
+python3 filter_reads.py $HOME/project_2/databases/working_ap $flowcell
