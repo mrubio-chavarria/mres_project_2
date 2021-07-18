f8ae474794ee04126a19998b977cf1ec6ea56b62
mrubio-chavarria <msrch@hotmail.es>
2021-07-18 17:20:34 +0100

    Update rename_reads.sh

--- databases/working_porcupine/rename_reads.sh
+++ databases/working_porcupine/rename_reads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=4:mem=4gb
 #PBS -lwalltime=01:30:00
-
+#PBS -J 1-4
 
 module load anaconda3/personal
 
@@ -9,7 +9,4 @@ source activate project2_venv
 
 cd $HOME/project_2/databases/working_porcupine
 
-for flowcell in flowcell1 flowcell2 flowcell3 flowcell4
-do
-  python3 rename_reads.py $HOME/project_2/databases/working_porcupine $flowcell
-done
+python3 rename_reads.py $HOME/project_2/databases/working_porcupine flowcell$PBS_ARRAY_INDEX
