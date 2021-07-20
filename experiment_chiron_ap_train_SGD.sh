46bff3f60799fbcd85d88fec81ce7659cac7610f
mrubio-chavarria <msrch@hotmail.es>
2021-07-20 19:28:36 +0100

    Update experiment_chiron_ap_train_RMSProp.py, experiment_chiron_ap_train_RMSProp.sh, and 2 more files...

---
+++ experiment_chiron_ap_train_SGD.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -lselect=1:ncpus=16:mem=96gb:ngpus=4
+#PBS -lwalltime=24:00:00
+
+# Load dependencies
+module load anaconda3/personal
+source activate project2_venv
+
+# Check GPUs
+echo "Available GPUs: $CUDA_VISIBLE_DEVICES"
+
+# Launch script
+echo "Launch script"
+python3 $HOME/project_2/experiment_chiron_ap_train_SGD.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap
