6c818c67f211a4597c5354f25a04215db6241a0b
mrubio-chavarria <msrch@hotmail.es>
2021-07-21 16:14:10 +0100

    Update experiment_chiron_3xr6_train_Adam.py

--- experiment_chiron_3xr6_train_Adam.py
+++ experiment_chiron_3xr6_train_Adam.py
@@ -204,12 +204,12 @@ if __name__ == "__main__":
     # # Training
     train(model, train_data, experiment, **training_parameters)
     
-    # # Save the model
-    # time = str(datetime.now()).replace(' ', '_')
-    # model_name = f'model_{time}.pt'
-    # model_path = database_dir + '/' + 'saved_models' + '/' + model_name
-    # torch.save(model.state_dict(), model_path)
-    # # experiment.log_model(f'model_{time}', model_path)
+    # Save the model
+    time = str(datetime.now()).replace(' ', '_')
+    model_name = f'model_{time}.pt'
+    model_path = database_dir + '/' + 'saved_models' + '/' + model_name
+    torch.save(model.state_dict(), model_path)
+    # experiment.log_model(f'model_{time}', model_path)
 
     # # test = list(train_data)[0]
     # # output = model(test['signals'])
