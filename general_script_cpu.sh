573d46c267b6c9bcbd6176afb50117f7b3007449
mrubio-chavarria <msrch@hotmail.es>
2021-07-25 11:55:20 +0100

    Update general_script_cpu.sh

--- general_script_cpu.sh
+++ general_script_cpu.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-#PBS -lselect=1:ncpus=16:mem=16gb
-#PBS -lwalltime=03:00:00
+#PBS -lselect=1:ncpus=4:mem=4gb
+#PBS -lwalltime=01:00:00
 
 # Load dependencies
 module load anaconda3/personal
 
 source activate project2_venv
 
-# Launch script
-for flowcell in flowcell1 flowcell2 flowcell3
-do
-    cd $HOME/project_2/databases/working_3xr6/reads/$flowcell
-    cp -R single_security single
-done
+cd $HOME/project_2
+
+y | conda install pandas
