68ce18e42f985caf85a72c1817f1108642f533d9
mrubio-chavarria <msrch@hotmail.es>
2021-07-25 16:57:41 +0100

    Update experiment_chiron_ap_train.py and experiment_chiron_ap_train.sh

--- experiment_chiron_ap_train.sh
+++ experiment_chiron_ap_train.sh
@@ -11,4 +11,4 @@ echo "Available GPUs: $CUDA_VISIBLE_DEVICES"
 
 # Launch script
 echo "Launch script"
-python3 $HOME/project_2/experiment_chiron_ap_train.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/ap_RMSprop_storage_4.tsv
+python3 $HOME/project_2/experiment_chiron_ap_train.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/ap_Adam_storage_3.tsv
