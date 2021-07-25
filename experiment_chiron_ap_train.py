e59ea0f38cb4bc15a8d8c9f2538e885d1b0b34d6
mrubio-chavarria <msrch@hotmail.es>
2021-07-25 16:20:29 +0100

    Update experiment_chiron_ap_train.py and experiment_chiron_ap_train.sh

--- experiment_chiron_ap_train.py
+++ experiment_chiron_ap_train.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
         'batch_size': batch_size,
         'learning_rate': 0.0001,
         'max_learning_rate': 1E-2,
-        'weight_decay': 0.01,
+        'weight_decay': 0,
         'momemtum': 0.9,
         'optimiser': 'RMSprop',
         'sequence_lengths': window_sizes,
