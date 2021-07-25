8971c84590b2def76a092759a9f908bc8b3334d8
mrubio-chavarria <msrch@hotmail.es>
2021-07-25 19:06:16 +0100

    Update experiment_chiron_ap_train.py and experiment_chiron_ap_train.sh

--- experiment_chiron_ap_train.sh
+++ experiment_chiron_ap_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=8:mem=48gb:ngpus=2
-#PBS -lwalltime=00:45:00
+#PBS -lwalltime=01:15:00
 
 # Load dependencies
 module load anaconda3/personal
@@ -11,4 +11,4 @@ echo "Available GPUs: $CUDA_VISIBLE_DEVICES"
 
 # Launch script
 echo "Launch script"
-python3 $HOME/project_2/experiment_chiron_ap_train.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/ap_SGD_storage_2.tsv
+python3 $HOME/project_2/experiment_chiron_ap_train.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/ap_Adam_storage_3.tsv
