e835de2a764e580954f2d1687efbb7d574225848
mrubio-chavarria <msrch@hotmail.es>
2021-07-26 19:25:26 +0100

    Update experiment3_3xr6.py and experiment3_3xr6.sh

--- experiment3_3xr6.sh
+++ experiment3_3xr6.sh
@@ -12,4 +12,4 @@ echo "Available GPUs: $CUDA_VISIBLE_DEVICES"
 # Launch script
 id=2
 echo "Launch script"
-python3 $HOME/project_2/experiment2_3xr6.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/3xr6_exp3_epoch_1_$id.tsv
+python3 $HOME/project_2/experiment3_3xr6.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/3xr6_exp3_epoch_1_$id.tsv
