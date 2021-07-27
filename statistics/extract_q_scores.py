c0cd083f77fe00a2aee1fc8ff7c156a435e6981b
mrubio-chavarria <msrch@hotmail.es>
2021-07-27 09:42:49 +0100

    Update extract_q_scores.py

--- statistics/extract_q_scores.py
+++ statistics/extract_q_scores.py
@@ -56,7 +56,7 @@ def extract_q_score(read_files, reference_file, workdir, n_processes=2):
 if __name__ == "__main__":
     
     workdir = sys.argv[1]
-    n_processes = sys.argv[2]
+    n_processes = int(sys.argv[2])
 
     if workdir.endswith('ap'):
         flowcells = ['flowcell1', 'flowcell2', 'flowcell3', 'flowcell4']
