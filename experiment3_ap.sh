e67b17d9ca4b21c32558492b23e1cb8fb5f6b856
mrubio-chavarria <msrch@hotmail.es>
2021-07-28 17:07:43 +0100

    Update experiment3_ap.py and experiment3_ap.sh

--- experiment3_ap.sh
+++ experiment3_ap.sh
@@ -11,4 +11,4 @@ echo "Available GPUs: $CUDA_VISIBLE_DEVICES"
 
 # Launch script
 echo "Launch script"
-python3 $HOME/project_2/experiment3_ap.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/ap_exp3_epoch_5.tsv
+python3 $HOME/project_2/experiment3_ap.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/ap_exp3_epoch_4.tsv
