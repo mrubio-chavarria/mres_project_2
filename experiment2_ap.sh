aa7bd355d8788b8d8ad40f9897e57f85fcbe5d90
mrubio-chavarria <msrch@hotmail.es>
2021-07-28 20:28:25 +0100

    Update experiment2_3xr6.py, experiment2_3xr6.sh, and 2 more files...

--- experiment2_ap.sh
+++ experiment2_ap.sh
@@ -11,5 +11,5 @@ echo "Available GPUs: $CUDA_VISIBLE_DEVICES"
 
 # Launch script
 echo "Launch script"
-id=13
+id=31
 python3 $HOME/project_2/experiment2_ap.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/ap_SGD_storage_$id.tsv
