3bb8a58f1f7e44f0087f474f7298be2dc3580c8c
mrubio-chavarria <msrch@hotmail.es>
2021-07-29 21:11:29 +0100

    Update experiment2_3xr6.sh, experiment2_ap.sh, and 4 more files...

--- experiment2_3xr6.sh
+++ experiment2_3xr6.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=8:mem=48gb:ngpus=2
 #PBS -lwalltime=24:00:00
+#PBS -J 0-5
 
 # Load dependencies
 module load anaconda3/personal
@@ -10,9 +11,8 @@ source activate project2_venv
 echo "Available GPUs: $CUDA_VISIBLE_DEVICES"
 
 # Check array index
-ID=0
-echo "ID: $ID"
+echo "Array job ID: $PBS_ARRAY_INDEX"
 
 # Launch script
 echo "Launch script"
-echo "python3 $HOME/project_2/experiment2_3xr6.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/3xr6_exp2_$ID.tsv $ID"
+python3 $HOME/project_2/experiment2_3xr6.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/3xr6_exp2_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX
--- experiment3_ap.py
+++ experiment3_ap.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     file_manual_record = sys.argv[3]
 
     # Number of initialisation epochs
-    n_initialisation_epochs = int(sys.argv[4]) - 1
+    n_initialisation_epochs = int(sys.argv[4])
 
     # Set fast5 and reference
     reference_file = database_dir + '/' + 'reference.fasta'
