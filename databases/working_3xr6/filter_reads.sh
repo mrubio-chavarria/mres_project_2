a8a3847cbcf2fda2213f486d401b3e1f81ad563e
mrubio-chavarria <msrch@hotmail.es>
2021-08-02 21:45:27 +0100

    Update filter_reads.sh

--- databases/working_3xr6/filter_reads.sh
+++ databases/working_3xr6/filter_reads.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=4:mem=4gb
 #PBS -lwalltime=12:00:00
-#PBS -J 1-3
 
 module load anaconda3/personal
 
@@ -9,9 +8,9 @@ source activate project2_venv
 
 cd $HOME/project_2/databases/working_3xr6
 
-flowcell=flowcell$PBS_ARRAY_INDEX
+flowcell=flowcell1
 
 # Launch the resquiggling for every folder
-echo "Job index: $PBS_ARRAY_INDEX"
+echo "Job index: 1"
 echo "Flocell: $flowcell"
 python3 filter_reads.py $HOME/project_2/databases/working_3xr6 $flowcell
