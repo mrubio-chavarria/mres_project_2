e71754e318810d997c6a0657ceb0a42f8f4094ba
mrubio-chavarria <msrch@hotmail.es>
2021-08-02 16:33:43 +0100

    Update experiment3_3xr6.py and experiment3_3xr6.sh

--- experiment3_3xr6.sh
+++ experiment3_3xr6.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=8:mem=48gb:ngpus=2
 #PBS -lwalltime=24:00:00
-#PBS -J 0-5
+
+PBS_ARRAY_INDEX=0
 
 # Load dependencies
 module load anaconda3/personal
@@ -15,4 +16,5 @@ echo "Array job ID: $PBS_ARRAY_INDEX"
 
 # Launch script
 echo "Launch script"
-python3 $HOME/project_2/experiment3_3xr6.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/3xr6_exp3_epoch_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX
+n_initialisation_epochs=0
+python3 $HOME/project_2/experiment3_3xr6.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/3xr6_exp3_epoch_$PBS_ARRAY_INDEX.tsv $n_initialisation_epochs
