4c6ca3aa916035d3409e83c5655e39781e38fb03
mrubio-chavarria <msrch@hotmail.es>
2021-08-03 11:44:06 +0100

    Update filter_reads.py

--- databases/working_3xr6/filter_reads.py
+++ databases/working_3xr6/filter_reads.py
@@ -9,9 +9,11 @@ perform once the basecalls have been obtained.
 # Libraries
 import os
 import sys
+from numpy.core.fromnumeric import mean
 from tombo import tombo_helper, tombo_stats, resquiggle
 import h5py
 import mappy
+from torch import threshold
 from tqdm import tqdm
 import multiprocessing as mp
 from multiprocessing import Manager
@@ -22,27 +24,36 @@ import csv
 
 # Functions
 def filter_reads(pair):
-    read_file, filtered_reads_ids = pair
+    read_file, reference_file, threshold = pair
     try:
         fast5_data = h5py.File(read_file, 'r')
         read_id = fast5_data['Raw']['Reads'][list(fast5_data['Raw']['Reads'].keys())[0]].attrs['read_id'].decode('UTF-8')
-        if read_id in filtered_reads_ids:
+        # Set parameters for resquiggling
+        aligner = mappy.Aligner(reference_file, preset=str('map-ont'), best_n=1)
+        seq_samp_type = tombo_helper.get_seq_sample_type(fast5_data)
+        std_ref = tombo_stats.TomboModel(seq_samp_type=seq_samp_type)
+        # Extract data from FAST5
+        mean_q_score = resquiggle.map_read(fast5_data, aligner, std_ref).mean_q_score
+        if mean_q_score >= threshold:
             filename = read_file.split('/')[-1]
             subfolder = '/'.join(read_file.split('/')[:-1])
             new_filename = subfolder + '/' + label + filename
+            print('Read renamed:', new_filename)
             os.rename(read_file, new_filename)
-    except OSError:
-        # Corrupted file, do not modify
+    except tombo_helper.TomboError:
         pass
+    except OSError:
+        pass
+
     
 
 if __name__ == "__main__":
     
-    workdir = sys.argv[1]
-    flowcell = sys.argv[2]
+    # workdir = sys.argv[1]
+    # flowcell = sys.argv[2]
     
-    # workdir = f'/home/mario/Projects/project_2/databases/working_3xr6'
-    # flowcell = 'flowcell3'
+    workdir = f'/home/mario/Projects/project_2/databases/working_3xr6'
+    flowcell = 'flowcell3'
 
     # Filter files below the q score threshold
     print('***************************************************************************************')
@@ -60,12 +71,13 @@ if __name__ == "__main__":
     q_score_threshold = 7.0
     filtered_reads = []
     subfolders = [single_reads_folder + '/' + subfolder for subfolder in os.listdir(single_reads_folder) if not subfolder.endswith('index') and not subfolder.endswith('txt')]
-    single_read_files = tuple((it, filtered_reads_ids) for sl in [[folder + '/' + file for file in os.listdir(folder)] for folder in subfolders] for it in sl)
+    reference_file = workdir + '/' + 'reference.fasta'
+    single_read_files = tuple((it, reference_file, q_score_threshold) for sl in [[folder + '/' + file for file in os.listdir(folder)] for folder in subfolders] for it in sl)
     reference_file = workdir + '/' + 'reference.fasta'
     print('Filter the selected reads')
     print('Number of reads:', len(single_read_files))
     # Filter the files
-    with Pool(8) as p:
+    with Pool(4) as p:
         p.map(filter_reads, single_read_files)
     print('High-quality reads marked')            
 
