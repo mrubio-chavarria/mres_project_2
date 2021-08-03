437f0870964cdc298b6a11c46e292ad8450a08c6
mrubio-chavarria <msrch@hotmail.es>
2021-08-03 11:14:53 +0100

    Update bnlstm.py

--- bnlstm.py
+++ bnlstm.py
@@ -425,9 +425,10 @@ def bnlstm_cell(x, h_t_1, c_t_1, weight_ih, weight_hh, bias, batch_norms, time):
     :return: hidden and cell states associated with this time step (h_t, c_t),
     both with dimensionality: [hidden_size, batch_size].
     """
-    w_hh_by_h_t_1 = weight_hh.cuda() @ h_t_1.cuda()
+    device = torch.device('cuda:0')
+    w_hh_by_h_t_1 = weight_hh.to(device) @ h_t_1.to(device)
     w_hh_by_h_t_1 = w_hh_by_h_t_1.permute(1, 0)
-    w_ih_by_x = weight_ih.cuda() @ x.cuda()
+    w_ih_by_x = weight_ih.to(device) @ x.to(device)
     w_ih_by_x = w_ih_by_x.permute(1, 0)
     ifgo = batch_norms[0](w_hh_by_h_t_1, time).permute(1, 0) + \
             batch_norms[1](w_ih_by_x, time).permute(1, 0) + bias
