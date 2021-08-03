5d59a7a81f7ed9f666d29282cb66553caf819297
mrubio-chavarria <msrch@hotmail.es>
2021-08-03 15:42:02 +0100

    Update filter_reads.py and filter_reads.sh

--- databases/working_3xr6/filter_reads.py
+++ databases/working_3xr6/filter_reads.py
@@ -76,12 +76,12 @@ def filter_reads(read_files, reference_file,  q_score_threshold, workdir, new_wa
 
 if __name__ == "__main__":
     
-    # workdir = sys.argv[1]
-    # flowcell = sys.argv[2]
+    workdir = sys.argv[1]
+    flowcell = sys.argv[2]
     
-    workdir = f'/home/mario/Projects/project_2/databases/working_3xr6'
-    flowcell = 'flowcell3'
-    job_index = 3
+    # workdir = f'/home/mario/Projects/project_2/databases/working_3xr6'
+    # flowcell = 'flowcell3'
+    # job_index = 3
 
     if workdir.endswith('ap'):
         # Filter files below the q score threshold
