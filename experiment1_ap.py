71023d31420ccd9372bba782f67025ec631e7d3f
mrubio-chavarria <msrch@hotmail.es>
2021-08-03 22:49:11 +0100

    Update experiment1_ap.py

--- experiment1_ap.py
+++ experiment1_ap.py
@@ -100,6 +100,10 @@ if __name__ == "__main__":
     validation_data = CustomisedDataLoader(dataset=validation_dataset, batch_size=batch_size, sampler=CustomisedSampler, collate_fn=collate_text2int_fn)
     validation_data = DataLoader(validation_dataset_300, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_text2int_fn)
 
+    print(list(train_data)[0]['sequences'])
+    print(len(list(train_data)[0]['sequences']))
+    raise OSError
+
     # Model
     # Parameters
     TCN_parameters = {
