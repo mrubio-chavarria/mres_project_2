1db56d51c1499dab9e685f740b9c4fa4eafc7e96
mrubio-chavarria <msrch@hotmail.es>
2021-08-04 10:43:47 +0100

    Update training_utils.py

--- training_utils.py
+++ training_utils.py
@@ -167,9 +167,6 @@ def launch_training(model, train_data, validation_data, device, **kwargs):
             target_sequences = batch['sequences']
             targets_lengths = torch.Tensor(batch['targets_lengths']).type(torch.int16).to(device)
             batch, target = batch['signals'].to(device), batch['targets'].to(device)
-            print(batch)
-            print(batch.shape)
-            raise OSError
             # All the sequences in a batch are of the same length
             sequences_lengths = torch.Tensor(tuple([batch.shape[-1]] * batch.shape[0])).type(torch.int16).to(device)
             if batch.shape[0] != batch_size:
