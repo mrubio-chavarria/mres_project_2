592276512f431f53a051309a1216fd99acb23043
mrubio-chavarria <msrch@hotmail.es>
2021-08-04 17:48:07 +0100

    Update general_script_cpu.sh

--- general_script_cpu.sh
+++ general_script_cpu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=4:mem=4gb
 #PBS -lwalltime=01:00:00
+#PBS -J 1-3
 
 # Load dependencies
 module load anaconda3/personal
@@ -9,4 +10,4 @@ source activate project2_venv
 
 cd $HOME/project_2/databases/working_3xr6
 
-tar -xf flowcell1.tar.gz
+tar -xf flowcell$PBS_ARRAY_INDEX.tar.gz
