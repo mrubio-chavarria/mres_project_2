05c6d11132a40756ed83c67255350d7ddc5e731f
mrubio-chavarria <msrch@hotmail.es>
2021-08-05 17:11:40 +0100

    Update move_3xr6_reads.py

--- statistics/move_3xr6_reads.py
+++ statistics/move_3xr6_reads.py
@@ -5,22 +5,25 @@ from shutil import copy
 
 home = sys.argv[1]
 
-route = home + "/project_2/nanopolish/working_ap/reads"
+route = home + "/project_2/nanopolish/working_3xr6/reads"
 
-new_route = home + "/project_2/nanopolish/working_ap/reads"
+new_route = home + "/project_2/nanopolish/working_3xr6/reads"
 
-files = []
-for flowcell in ['flowcell1', 'flowcell2', 'flowcell3', 'flowcell4']:
+
+for flowcell in ['flowcell1', 'flowcell2', 'flowcell3']:
     folder = route + '/' + flowcell
-    for idx, file in enumerate(os.listdir(folder)):
-        if file.endswith('fast5'):
-            old_file = folder + '/' + file
-            files.append(file)
-            new_file = new_route + '/' + flowcell + '_' + str(idx) + '_' + file
-            try:
-                copy(old_file, new_file)
-            except FileNotFoundError:
-                continue
+    for subfolder in os.listdir(folder):
+        if subfolder.endswith('txt') or subfolder.endswith('index'):
+            continue
+        subfolder = folder + '/' + subfolder
+        for idx, file in enumerate(os.listdir(subfolder)):
+            if file.endswith('fast5'):
+                old_file = folder + '/' + file
+                new_file = new_route + '/' + flowcell + '_' + subfolder + '_' + str(idx) + '_' + file
+                try:
+                    copy(old_file, new_file)
+                except FileNotFoundError:
+                    continue
 
 
 
