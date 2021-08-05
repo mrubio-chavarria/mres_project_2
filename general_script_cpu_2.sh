097d2a575d2b9ec19d8af95c27e0bc9f4f2967ff
mrubio-chavarria <msrch@hotmail.es>
2021-08-05 16:03:38 +0100

    Update general_script_cpu_2.sh

---
+++ general_script_cpu_2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=8:mem=24gb
+#PBS -l walltime=12:00:00
+
+module load anaconda3/personal
+
+source activate nanopolish_venv
+
+cd $HOME/project_2/nanopolish/working_ap
+
+flye --nano-raw reads.fasta --out-dir assembly --threads 4  --iterations 0
