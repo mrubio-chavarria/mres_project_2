9597a30e72d87dd5daf087a3b8a5fd53c5bc513e
mrubio-chavarria <msrch@hotmail.es>
2021-08-06 08:03:42 +0100

    Update move_3xr6_reads.py

--- statistics/move_3xr6_reads.py
+++ statistics/move_3xr6_reads.py
@@ -11,6 +11,7 @@ new_route = workdir + "/nanopolish/working_3xr6"
 
 
 for flowcell in ['flowcell1', 'flowcell2', 'flowcell3']:
+    print(f'Processing {flowcell}')
     folder = workdir + '/databases/working_3xr6/reads' + '/' + flowcell + '/' + 'single'
     for subfolder in os.listdir(folder):
         if subfolder.endswith('txt') or subfolder.endswith('index'):
@@ -18,11 +19,12 @@ for flowcell in ['flowcell1', 'flowcell2', 'flowcell3']:
         subfolder = folder + '/' + subfolder
         for idx, file in enumerate(os.listdir(subfolder)):
             if file.endswith('fast5'):
-                old_file = folder + '/' + file
+                old_file = subfolder + '/' + file
                 new_file = new_route + '/' + flowcell + '_' + subfolder + '_' + str(idx) + '_' + file
                 try:
                     copy(old_file, new_file)
                 except FileNotFoundError:
+                    print(f'File {old_file} not found')
                     continue
 
 
