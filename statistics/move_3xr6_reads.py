374781d86ac9b114fba1d09b64f8cf4f89ee70bd
mrubio-chavarria <msrch@hotmail.es>
2021-08-06 17:58:09 +0100

    Update move_3xr6_reads.py

--- statistics/move_3xr6_reads.py
+++ statistics/move_3xr6_reads.py
@@ -13,15 +13,15 @@ base_folder = workdir + "/nanopolish/working_3xr6/reads"
 
 for flowcell in ['flowcell1', 'flowcell2', 'flowcell3']:
     print(f'Processing {flowcell}')
-    folder = workdir + '/databases/working_3xr6/reads' + '/' + flowcell
-    for subfolder in os.listdir(folder):
+    folder = base_folder + '/' + flowcell
+    for idx1, subfolder in enumerate(os.listdir(folder)):
         if subfolder.endswith('txt') or subfolder.endswith('index'):
             continue
         subfolder = folder + '/' + subfolder
         for idx, file in enumerate(os.listdir(subfolder)):
             if file.endswith('fast5'):
                 old_file = subfolder + '/' + file
-                new_file = base_folder + '/' + str(uuid4()) + '_' + file
+                new_file = base_folder + '/' + flowcell + '_' + str(idx1) + str(idx) + '_' + file
                 try:
                     print(f'New file: {new_file}')
                     print(f'Old file: {old_file}')
