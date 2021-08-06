5498b311449acf4a4d38bfa17c234b310698b978
mrubio-chavarria <msrch@hotmail.es>
2021-08-06 13:34:30 +0100

    Update experiment1_3xr6.py and experiment1_ap.py

--- experiment1_ap.py
+++ experiment1_ap.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     # Load the train dataset
     train_window_sizes = [200, 400, 1000]
     train_max_reads = 668  # Select all the reads
-    train_max_batches = 1250
+    train_max_batches = 2500
     train_max_windows = int(train_max_batches * (batch_size + 1))
     train_folder = database_dir + '/' + 'train_reads'
     
