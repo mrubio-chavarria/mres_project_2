8256dc488831e46e73c08ffec0f0b091e8cecd4e
mrubio-chavarria <msrch@hotmail.es>
2021-08-06 15:53:46 +0100

    Update move_3xr6_reads.py

--- statistics/move_3xr6_reads.py
+++ statistics/move_3xr6_reads.py
@@ -7,12 +7,12 @@ home = sys.argv[1]
 
 workdir = home + "/project_2"
 
-new_route = workdir + "/nanopolish/working_3xr6"
+base_folder = workdir + "/nanopolish/working_3xr6/reads"
 
 
 for flowcell in ['flowcell1', 'flowcell2', 'flowcell3']:
     print(f'Processing {flowcell}')
-    folder = workdir + '/databases/working_3xr6/reads' + '/' + flowcell + '/' + 'single'
+    folder = workdir + '/databases/working_3xr6/reads' + '/' + flowcell
     for subfolder in os.listdir(folder):
         if subfolder.endswith('txt') or subfolder.endswith('index'):
             continue
@@ -20,7 +20,7 @@ for flowcell in ['flowcell1', 'flowcell2', 'flowcell3']:
         for idx, file in enumerate(os.listdir(subfolder)):
             if file.endswith('fast5'):
                 old_file = subfolder + '/' + file
-                new_file = new_route + '/' + flowcell + '_' + subfolder.split('/')[-1] + '_' + str(idx) + '_' + file
+                new_file = base_folder + '/' + flowcell + '_' + subfolder.split('/')[-1] + '_' + str(idx) + '_' + file
                 try:
                     copy(old_file, new_file)
                 except FileNotFoundError as e:
