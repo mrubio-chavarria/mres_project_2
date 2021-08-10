35e94d945bba61c6a9c1fe26b1b397f8886d3c4a
mrubio-chavarria <msrch@hotmail.es>
2021-08-10 20:58:14 +0100

    Update general_script_cpu.sh

--- general_script_cpu.sh
+++ general_script_cpu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#PBS -lselect=1:ncpus=8:mem=32gb
-#PBS -lwalltime=10:00:00
+#PBS -lselect=1:ncpus=32:mem=64gb
+#PBS -lwalltime=24:00:00
 
 # Load dependencies
 module load anaconda3/personal
 
 source activate nanopolish_venv
 
-cd $HOME/project_2/nanopolish/working_3xr6
+cd $HOME/project_2/nanopolish/working_ap
 
-canu -p 3xr6 -d canu_assembly genomeSize=5m -nanopore multi.fastq minInputCoverage=0 stopOnLowCoverage=0 gridOptions="-l walltime=10:00:00"
+canu -p ap -d canu_assembly genomeSize=5m -nanopore multi.fastq minInputCoverage=0 stopOnLowCoverage=0 useGrid=false
