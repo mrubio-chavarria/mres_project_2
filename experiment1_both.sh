eafa469f61eed1e9c3e565f5d2184f23fedcc8da
mrubio-chavarria <msrch@hotmail.es>
2021-08-10 15:33:27 +0100

    Update experiment1_both.py and experiment1_both.sh

---
+++ experiment1_both.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -lselect=1:ncpus=16:mem=96gb:ngpus=4
+#PBS -lwalltime=24:00:00
+PBS_ARRAY_INDEX=1
+
+# Load dependencies
+module load anaconda3/personal
+source activate project2_venv
+
+# Check GPUs
+echo "Available GPUs: $CUDA_VISIBLE_DEVICES"
+
+# Check array index
+echo "Array job ID: $PBS_ARRAY_INDEX"
+
+# Launch script
+echo "Launch script"
+python3 $HOME/project_2/experiment1_3xr6.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/complete_3xr6_exp1_gamma_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX
