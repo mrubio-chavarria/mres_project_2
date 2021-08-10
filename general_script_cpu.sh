5faa0dd248c5105587e2eb2140c553ea07e743ba
mrubio-chavarria <msrch@hotmail.es>
2021-08-10 15:52:14 +0100

    Update general_script_cpu.sh

--- general_script_cpu.sh
+++ general_script_cpu.sh
@@ -9,4 +9,4 @@ source activate nanopolish_venv
 
 cd $HOME/project_2/nanopolish/working_3xr6
 
-canu -p 3xr6 -d canu_assembly genomeSize=5m -nanopore multi.fastq minInputCoverage=0 stopOnLowCoverage=0 useGrid=false
+canu -p 3xr6 -d canu_assembly genomeSize=5m -nanopore multi.fastq minInputCoverage=0 stopOnLowCoverage=0 gridOptions="-l walltime=10:00:00"
