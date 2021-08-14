b6fefb7863efef14237625f9a62acc0a430af99b
mrubio-chavarria <msrch@hotmail.es>
2021-08-14 16:29:44 +0100

    Update experiment1_ap.sh

--- experiment1_ap.sh
+++ experiment1_ap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=16:mem=96gb:ngpus=4
 #PBS -lwalltime=24:00:00
-PBS_ARRAY_INDEX=1
+PBS_ARRAY_INDEX=2
 
 # Load dependencies
 module load anaconda3/personal
@@ -15,4 +15,4 @@ echo "Array job ID: $PBS_ARRAY_INDEX"
 
 # Launch script
 echo "Launch script"
-python3 $HOME/project_2/experiment1_ap.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/complete_ap_exp1_gamma_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX
+python3 $HOME/project_2/experiment1_ap.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/complete_ap_exp1_gamma_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX $HOME/project_2/databases/working_ap/saved_models/0_model_2021-08-14_14:15:34.497265_3323e367-2fa9-4c62-8256-5a67d38ba2fc.pt
