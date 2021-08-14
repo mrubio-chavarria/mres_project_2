84567a16869470bb02ebdd4de80ac226f4a88b3d
mrubio-chavarria <msrch@hotmail.es>
2021-08-14 07:21:26 +0100

    Update experiment1_3xr6.sh and experiment1_both.sh

--- experiment1_3xr6.sh
+++ experiment1_3xr6.sh
@@ -15,4 +15,4 @@ echo "Array job ID: $PBS_ARRAY_INDEX"
 
 # Launch script
 echo "Launch script"
-python3 $HOME/project_2/experiment1_3xr6.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/complete_3xr6_exp1_gamma_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX
+python3 $HOME/project_2/experiment1_3xr6.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/complete_3xr6_exp1_gamma_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX /rds/general/user/mr820/home/project_2/databases/working_3xr6/saved_models/0_model_2021-08-14_01:06:02.046298_3bc9935b-8a8b-4ff0-8702-e9d96abed80f.pt
--- experiment1_both.sh
+++ experiment1_both.sh
@@ -15,4 +15,4 @@ echo "Array job ID: $PBS_ARRAY_INDEX"
 
 # Launch script
 echo "Launch script"
-python3 $HOME/project_2/experiment1_both.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/databases/working_3xr6 $HOME/project_2/complete_both_exp1_gamma_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX
+python3 $HOME/project_2/experiment1_both.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_ap $HOME/project_2/databases/working_3xr6 $HOME/project_2/complete_both_exp1_gamma_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX /rds/general/user/mr820/home/project_2/databases/working_both/saved_models/0_model_2021-08-14_01:27:41.583473_270e8f7d-40e2-4fdb-b813-e9e6110c0323.pt
