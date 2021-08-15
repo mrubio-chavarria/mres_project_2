71dd85f903c9de11862e18b00cc30394c2aef647
mrubio-chavarria <msrch@hotmail.es>
2021-08-15 07:38:30 +0100

    Update experiment1_3xr6_all.sh

--- experiment1_3xr6_all.sh
+++ experiment1_3xr6_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=16:mem=96gb:ngpus=4
 #PBS -lwalltime=24:00:00
-PBS_ARRAY_INDEX=2
+PBS_ARRAY_INDEX=3
 
 # Load dependencies
 module load anaconda3/personal
@@ -15,4 +15,4 @@ echo "Array job ID: $PBS_ARRAY_INDEX"
 
 # Launch script
 echo "Launch script"
-python3 $HOME/project_2/experiment1_3xr6_all.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/complete_all_3xr6_exp1_gamma_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX $HOME/project_2/databases/working_3xr6/saved_models/0_model_2021-08-14_18:49:05.821805_4f8c37c9-6dc6-496d-b0e5-b3dadb5345ba.pt
+python3 $HOME/project_2/experiment1_3xr6_all.py $CUDA_VISIBLE_DEVICES $HOME/project_2/databases/working_3xr6 $HOME/project_2/complete_all_3xr6_exp1_gamma_$PBS_ARRAY_INDEX.tsv $PBS_ARRAY_INDEX $HOME/project_2/databases/working_3xr6/saved_models/1_model_2021-08-14_18:49:05.821805_4f8c37c9-6dc6-496d-b0e5-b3dadb5345ba.pt
